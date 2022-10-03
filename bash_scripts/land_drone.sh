e71ac4c5536cb54d19d7692b5ab860f1672644dd
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-10-03 13:48:24 +0200

    Added script for shutting down the velocity controller and landing the drone
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

---
+++ bash_scripts/land_drone.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /opt/ros/noetic/setup.bash
+source ~/catkin_ws/devel/setup.bash 
+
+rosservice call /velocity_controller/service/enable_controller "data: false" # Disabling the velocity-controller
+rostopic pub /anafi/cmd_land std_msgs/Empty "{}" # Land drone
