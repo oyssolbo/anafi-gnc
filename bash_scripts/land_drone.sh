8b4a30b18057858300a079e3907be56b41b2e7ae
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-10-04 08:26:55 +0200

    Cleanup
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- bash_scripts/land_drone.sh
+++ bash_scripts/land_drone.sh
@@ -3,5 +3,5 @@
 source /opt/ros/noetic/setup.bash
 source ~/catkin_ws/devel/setup.bash 
 
-rosservice call /velocity_controller/service/enable_controller "data: false" # Disabling the velocity-controller
-rostopic pub /anafi/cmd_land std_msgs/Empty "{}" # Land drone
+rosservice call /velocity_controller/service/enable_controller "data: false"    # Disabling the velocity-controller
+rostopic pub /anafi/cmd_land std_msgs/Empty "{}"                                # Land drone
