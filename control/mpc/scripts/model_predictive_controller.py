150d8502c01fac28217160208aa667bc5978dcde
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-10-04 16:50:41 +0200

    Add development for MPC using casadi
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- control/mpc/scripts/model_predictive_controller.py
+++ control/mpc/scripts/model_predictive_controller.py
@@ -2,18 +2,17 @@
 
 import numpy as np
 import casadi
+from scipy.spatial.transform import Rotation
+import do_mpc
 
 import rospy
 import std_msgs.msg
 
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import TwistStamped, Vector3Stamped, QuaternionStamped
 from olympe_bridge.msg import AttitudeCommand
 from std_srvs.srv import SetBool, SetBoolResponse
 
 import mpc_helpers.utilities as utilities
-
-import warnings
-warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 class ModelPredictiveController():
 
@@ -31,6 +30,8 @@ class ModelPredictiveController():
     rospy.Service("/mpc/service/enable_controller", SetBool, self.__enable_controller)
 
     # Setup subscribers 
+    rospy.Subscriber("/anafi/optical_flow_velocities", Vector3Stamped, self.__optical_flow_velocities_cb)
+    rospy.Subscriber("/anafi/attitude", QuaternionStamped, self.__attitude_cb)
     # Attitude estimate from the anafi
     # Optical flow feedback from the bridge
     # Position estimate from the EKF
@@ -39,24 +40,14 @@ class ModelPredictiveController():
     self.attitude_ref_pub = rospy.Publisher("/anafi/cmd_rpyt", AttitudeCommand, queue_size=1)
 
     # Initial values
-    self.guidance_reference_velocities : np.ndarray = np.zeros((3, 1))
-    self.velocities_body : np.ndarray = np.zeros((3, 1))
+    self.optical_velocities : np.ndarray = np.zeros((3, 1))
+    self.attitude_rotation_matrix : np.ndarray = np.eye(3)
+    self.attitude_rpy : np.ndarray = np.zeros((3, 1))
 
-    self.velocities_body_timestamp : std_msgs.msg.Time = None
-    self.guidance_timestamp : std_msgs.msg.Time = None
+    self.optical_velocities_timestamp : std_msgs.msg.Time = None
+    self.attitude_timestamp : std_msgs.msg.Time = None 
 
     self.is_controller_active : bool = False
-
-
-  def __reference_velocities_cb(self, msg : TwistStamped):
-    msg_timestamp = msg.header.stamp
-
-    if not utilities.is_new_msg_timestamp(self.guidance_timestamp, msg_timestamp):
-      # Old message
-      return
-
-    self.guidance_timestamp = msg_timestamp
-    self.guidance_reference_velocities = np.array([msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z]).T
 
 
   def __enable_controller(self, msg : SetBool):
@@ -68,19 +59,105 @@ class ModelPredictiveController():
     return res 
 
 
-  def __optical_flow_velocities_cb(self, msg : TwistStamped) -> None:
+  def __optical_flow_velocities_cb(self, msg : Vector3Stamped) -> None:
     msg_timestamp = msg.header.stamp
 
-    if not utilities.is_new_msg_timestamp(self.velocities_body_timestamp, msg_timestamp):
+    if not utilities.is_new_msg_timestamp(self.optical_velocities_timestamp, msg_timestamp):
       # Old message
       return
     
-    self.velocities_body_timestamp = msg_timestamp
-    self.velocities_body = np.array([msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z]).T
+    self.optical_velocities_timestamp = msg_timestamp
+    self.optical_velocities = np.array([msg.vector.x, msg.vector.y, msg.vector.z]).T
 
+  
+  def __attitude_cb(self, msg : QuaternionStamped) -> None:
+    msg_timestamp = msg.header.stamp
+
+    if not utilities.is_new_msg_timestamp(self.attitude_timestamp, msg_timestamp):
+      # Old message
+      return
+    
+    self.attitude_timestamp = msg_timestamp
+    rotation = Rotation.from_quat([msg.quaternion.x, msg.quaternion.y, msg.quaternion.z, msg.quaternion.w])
+    self.attitude_rotation_matrix = rotation.as_matrix()
+    self.attitude_rpy = rotation.as_euler('xyz')
+
+
+  def __ode(self, x ,u) -> None:
+    drone_mass = 0.320
+    gravitational_acceleration = 9.82179
+    # Linear drag parameters from Martin Falang
+    linear_drag_x = 0.08063504
+    linear_drag_y = 0.09929089
+    linear_drag_matrix = np.diag([linear_drag_x, linear_drag_y, 0.0])
+
+    g_ned = np.array([[0], [0], [gravitational_acceleration]])
+
+    state_velocity = np.array(
+      [
+        [x[3]], 
+        [x[4]], 
+        [x[5]]
+      ]
+    )
+    rotation_matrix = Rotation.from_euler("xyz", np.array([x[6], x[7], x[8]])).as_matrix()
+
+    d_pos = self.attitude_rotation_matrix @ np.array(
+      [
+        [x[3]], 
+        [x[4]], 
+        [x[5]]
+      ]
+    )
+    d_vel = (rotation_matrix @ g_ned - linear_drag_matrix @ state_velocity) / (drone_mass)
+    d_vel[2] = -u[3] # Change in velocity defined in NED, while the thrust is in ENU
+
+    # Random values - must be estimated later with enough data (preferable from real world experiments)
+    k_phi = 1
+    k_theta = 1
+    tau_phi = 0.01
+    tau_theta = 0.01
+
+    d_rpy = np.array(
+      [
+        [(k_phi * u[0] - x[6]) / tau_phi], 
+        [(k_theta * u[1] - x[7]) / tau_theta], 
+        [u[3]]
+      ]
+    )
+    dx = np.vstack([d_pos, d_vel, d_rpy])
+    return casadi.vertcat(*dx)
+
+  def __initial_states(self) -> tuple:#[np.ndarray, np.ndarray]:
+    x0 = [0] * 9 #np.zeros((9, 1)) # Make 9 into a variable
+    u0 = [0] * 4 # np.zeros((4, 1)) # Make 4 into a variable
+    return x0, u0 
+
+  def __tuning_matrices(self) -> tuple:
+    Q = casadi.MX()
+
+  def __ineq_constraints(self) -> tuple:
+    x_min = np.array([]) 
+    x_max = np.array([])
+    u_min = np.array([-10 * np.pi / 180.0, -10 * np.pi / 180.0, -30 * np.pi / 180.0, -0.1]) # Config file
+    u_max = -1 * u_min
+    return x_min, x_max, u_min, u_max
+
+  def __tuning_parameters(self) -> list:
+    q = [2, 2, 0.5, 0.25, 0.25, 0.1, 0.5, 0.5, 0.01]
+    r = [1, 1, 1, 1]
+    s = [1, 1, 0.01, 1, 1, 0.5, 0.5, 0.5, 1]
+    return q, r, s
 
   def spin(self) -> None:
     while not rospy.is_shutdown():
+
+
+            
+      import os 
+      import sys
+      sys.exit(0)
+
       self.rate.sleep()
 
 
---
+++ control/mpc/scripts/mpc_helpers/do_mpc.py
@@ -0,0 +1,154 @@
+from cmath import inf
+import xdrlib
+import casadi 
+import do_mpc 
+import numpy as np
+
+from scipy.spatial.transform import Rotation
+
+def ode(x : casadi.MX, u : casadi.MX):
+  # Get these from a config-file
+  drone_mass = 0.320
+  gravitational_acceleration = 9.82179
+  linear_drag_x = 0.08063504
+  linear_drag_y = 0.09929089
+
+  # Random values - must be estimated later with enough data (preferable from real world experiments)
+  k_phi = 1
+  k_theta = 1
+  tau_phi = 0.01
+  tau_theta = 0.01
+
+  linear_drag_matrix = np.diag([linear_drag_x, linear_drag_y, 0.0])
+  g_ned = np.array([[0], [0], [gravitational_acceleration]])
+
+  # Extracting current states
+  velocity_body = np.array(
+    [
+      [x[3]], 
+      [x[4]], 
+      [x[5]]
+    ]
+  )
+  rotation_matrix_body_to_vehicle = Rotation.from_euler("xyz", np.array([x[6], x[7], x[8]])).as_matrix()
+  #print(rotation_matrix_body_to_vehicle) # Rotation matrix may be NAN TODO
+
+  # Derivatives
+  d_pos = rotation_matrix_body_to_vehicle @ np.array(
+    [
+      [x[3]], 
+      [x[4]], 
+      [x[5]]
+    ]
+  )
+  d_horizontal_vel = np.eye(2, 3) @ (((rotation_matrix_body_to_vehicle.T @ g_ned) - linear_drag_matrix @ velocity_body) / (drone_mass))
+  d_vertical_vel = -u[3] # Change in velocity defined in NED, while the thrust is in ENU
+
+  d_rpy = np.array(
+    [
+      [(k_phi * u[0] - x[6]) / tau_phi], 
+      [(k_theta * u[1] - x[7]) / tau_theta], 
+      [u[2]]
+    ]
+  )
+  return casadi.vertcat(*[d_pos, d_horizontal_vel, d_vertical_vel, d_rpy])
+
+
+def create_model() -> do_mpc.model.Model:
+  model_type = "continuous"
+  model = do_mpc.model.Model(model_type)
+
+  x = model.set_variable(var_type='_x', var_name='x', shape=(9,1))
+  u = model.set_variable(var_type='_u', var_name='u', shape=(4,1))
+
+  model.set_rhs('x', ode(x, u))
+  model.setup()
+
+  return model
+
+
+def create_objective_function(x, u):
+  # Config file - TODO
+  q = [2, 2, 0.5, 0.25, 0.25, 0.1, 0.5, 0.5, 0.01]
+  r = [1, 1, 0.5, 1]
+  
+  xTQ = casadi.mtimes(casadi.transpose(x), casadi.SX(np.diag(q)))
+  xTQx = casadi.mtimes(xTQ, x)
+
+  # uTR = casadi.mtimes(casadi.transpose(u), casadi.SX(np.diag(r)))
+  # uTRu = casadi.mtimes(uTR, u)
+
+  lagrange_term = xTQx
+  meyer_term = xTQx
+  # r_term = uTRu
+  r_term = r
+
+  return lagrange_term, r_term, meyer_term
+
+
+def create_mpc(model : do_mpc.model.Model) -> do_mpc.controller.MPC:
+  mpc = do_mpc.controller.MPC(model)
+
+  # https://www.do-mpc.com/en/latest/api/do_mpc.controller.MPC.set_param.html
+  setup_mpc = {
+      'n_horizon': 10,                  # Prediction horizon
+      'n_robust': 0,                    # Robust horizon for a robust scenario MPC
+      'open_loop': False,               # False will generate an individual cntrol input for each scenario. 
+                                        # True will use the same control input for all scenarios 
+      't_step': 0.2,                    # Time step of the MPC
+      'use_terminal_bounds': False,     # Bool indicating if terminal constraints are used
+      'collocation_deg': 4,             # Degree of collocation for continous models - https://www.do-mpc.com/en/latest/theory_orthogonal_collocation.html
+      'nl_cons_single_slack': False,    # True will use slacking variables for soft constraints 
+      'store_full_solution': False,
+      'store_lagr_multiplier': False,
+      'nlpsol_opts': {
+        'ipopt.linear_solver': 'MA27',  # Efficient linear solver
+        'ipopt.print_level': 0,         # Suppress output
+        'ipopt.sb': 'yes', 
+        'print_time': 0
+      }
+  }
+  mpc.set_param(**setup_mpc)
+  lagrange_term, r_term, meyer_term = create_objective_function(
+    x=model["x"], 
+    u=model["u"]
+  )
+
+  # Configure objective function:
+  mpc.set_objective(lterm=lagrange_term, mterm=meyer_term)
+  mpc.set_rterm(u = np.array(r_term)) 
+
+  # State and input bounds - use a config file for this
+  mpc.bounds['lower', '_x', 'x'] = [-inf, -inf, -inf, -2, -2, -0.1, -10*np.pi/180.0, -10*np.pi/180.0, -inf]
+  mpc.bounds['upper', '_x', 'x'] = [inf, inf, inf, 2, 2, 0.1, 10*np.pi/180.0, 10*np.pi/180.0, inf]
+  
+  mpc.bounds['lower', '_u', 'u'] = [-10*np.pi/180.0, -10*np.pi/180.0, -25*np.pi/180.0, -1.0]
+  mpc.bounds['upper', '_u', 'u'] = [10*np.pi/180.0, 10*np.pi/180.0, 25*np.pi/180.0, 1.0]
+
+  mpc.setup()
+
+  return mpc
+
+
+
+if __name__ == '__main__':  
+  model = create_model()  
+  mpc = create_mpc(model=model)
+
+  x0 = np.ones((9, 1))
+  u0 = np.zeros((4, 1))
+
+  y = np.zeros((9, 1))
+  dt = 0.02
+  import datetime
+  while True:
+    
+    x0 = x0 + dt * np.random.normal(np.zeros((9, 1)), dt * np.ones((9, 1)))
+    first_time = datetime.datetime.now()
+    print(mpc.solve())
+    u0 = mpc.make_step(x0)
+    later_time = datetime.datetime.now()
+    print(x0)
+    print(u0)
+    print(later_time - first_time)
+    print()
