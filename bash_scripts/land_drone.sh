18f6fac59dfd14b7302dc5c07203a4d6cb202c61
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-10-10 09:49:52 +0200

    Add MPC into the bash-script, and outcomment the velocity controller
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- bash_scripts/land_drone.sh
+++ bash_scripts/land_drone.sh
@@ -3,5 +3,6 @@
 source /opt/ros/noetic/setup.bash
 source ~/catkin_ws/devel/setup.bash 
 
-rosservice call /velocity_controller/service/enable_controller "data: false"    # Disabling the velocity-controller
+# rosservice call /velocity_controller/service/enable_controller "data: false"    # Disabling the velocity-controller
+rosservice call /mpc/service/enable_controller "data: false"                    # Disabling the mpc-controller
 rostopic pub /anafi/cmd_land std_msgs/Empty "{}"                                # Land drone
