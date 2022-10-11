8a69f398552bd29feeaef2ca2a7bbb2c3d4f18b4
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-10-11 18:47:31 +0200

    Added bash-script for switching to manual control and landing
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- bash_scripts/land_drone.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/noetic/setup.bash
-source ~/catkin_ws/devel/setup.bash 
-
-# rosservice call /velocity_controller/service/enable_controller "data: false"    # Disabling the velocity-controller
-rosservice call /mpc/service/enable_controller "data: false"                    # Disabling the mpc-controller
-rostopic pub /anafi/cmd_land std_msgs/Empty "{}"                                # Land drone
