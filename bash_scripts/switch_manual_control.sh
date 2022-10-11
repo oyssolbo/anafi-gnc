d9068d4cbcc4e622910fd6fa5533441edb8255a7
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-10-11 18:51:42 +0200

    Remove land-command
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- bash_scripts/switch_manual_control.sh
+++ bash_scripts/switch_manual_control.sh
@@ -5,6 +5,6 @@ source ~/catkin_ws/devel/setup.bash
 
 # rosservice call /velocity_controller/service/enable_controller "data: false"    # Disabling the velocity-controller
 rosservice call /mpc/service/enable_controller "data: false"                      # Disabling the mpc-controller
-rostopic pub -1 /anafi/cmd_offboard std_msgs/Bool "data: true"                    # Switch to manual control 
-rostopic pub -1 /anafi/cmd_land std_msgs/Empty "{}"                               # Switch to manual control 
+rostopic pub /anafi/cmd_offboard std_msgs/Bool "data: true"                    # Switch to manual control 
+# rostopic pub /anafi/cmd_land std_msgs/Empty "{}"                               # Land 
 
