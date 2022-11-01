59fed2cdb2b8f979a2dbf15dc7738f9e6f22d289
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-11-01 13:10:02 +0100

    Add ned-message from GNSS and use the message inside the mpc
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- control/mpc/scripts/model_predictive_controller.py
+++ control/mpc/scripts/model_predictive_controller.py
@@ -7,7 +7,7 @@ from scipy.spatial.transform import Rotation
 import rospy
 import std_msgs.msg
 
-from geometry_msgs.msg import TwistStamped, Vector3Stamped, QuaternionStamped
+from geometry_msgs.msg import TwistStamped, Vector3Stamped, QuaternionStamped, PointStamped
 from olympe_bridge.msg import AttitudeCommand
 from std_srvs.srv import SetBool, SetBoolResponse
 from anafi_uav_msgs.msg import PointWithCovarianceStamped
@@ -28,7 +28,7 @@ class ModelPredictiveController():
 
     # Setting up the controller
     self.mpc_parameters = rospy.get_param("~mpc_config")
-    self.MPC_solver = mpc.MPCSolver(mpc_parameters=self.mpc_parameters).get_mpc_solver()
+    self.mpc_solver = mpc.MPCSolver(mpc_parameters=self.mpc_parameters).get_mpc_solver()
 
     # Initial values
     self.nx = self.mpc_parameters["tuning_parameters"]["nx"]
@@ -41,7 +41,9 @@ class ModelPredictiveController():
 
     self.velocities_timestamp : std_msgs.msg.Time = None
     self.attitude_timestamp : std_msgs.msg.Time = None 
-    self.ekf_timestamp : std_msgs.msg.Time = None
+    self.position_timestamp : std_msgs.msg.Time = None
+    
+    self.last_rotation_matrix_body_to_vehicle : np.ndarray = None
 
     self.is_controller_active : bool = False
 
@@ -57,15 +59,22 @@ class ModelPredictiveController():
       rospy.loginfo("Node using polled velocity estimates as feedback")
       rospy.Subscriber("/anafi/polled_body_velocities", TwistStamped, self._polled_velocities_cb)
 
+    self.use_ned_pos_from_gnss : bool = rospy.get_param("/use_ned_pos_from_gnss")
+    if self.use_ned_pos_from_gnss:
+      rospy.loginfo("Node using position estimates from GNSS. Estimates from EKF disabled")
+      rospy.Subscriber("/anafi/ned_pos_from_gnss", PointStamped, self._ned_pos_cb)
+    else:
+      rospy.loginfo("Node using position estimates from EKF. Position estimates from GNSS disabled")
+      rospy.Subscriber("/estimate/ekf", PointWithCovarianceStamped, self._ekf_cb)
+
     rospy.Subscriber("/anafi/attitude", QuaternionStamped, self._attitude_cb)
-    rospy.Subscriber("/estimate/ekf", PointWithCovarianceStamped, self._ekf_cb)
 
     # Setup publishers
     self.attitude_ref_pub = rospy.Publisher("/anafi/cmd_rpyt", AttitudeCommand, queue_size=1)
 
     # Initializing the solver with zero-input
-    rospy.logdebug("Initializing the MPC")
-    self.MPC_solver.make_step(np.zeros((self.nx, self.m)))
+    rospy.logdebug("Initializing MPC")
+    self.mpc_solver.make_step(np.zeros((self.nx, self.m)))
     rospy.loginfo("MPC initialized")
 
 
@@ -103,12 +112,31 @@ class ModelPredictiveController():
   def _ekf_cb(self, msg : PointWithCovarianceStamped) -> None:
     msg_timestamp = msg.header.stamp
 
-    if not utilities.is_new_msg_timestamp(self.ekf_timestamp, msg_timestamp):
+    if not utilities.is_new_msg_timestamp(self.position_timestamp, msg_timestamp):
       # Old message
       return
     
-    self.ekf_timestamp = msg_timestamp
+    self.position_timestamp = msg_timestamp
     self.position = -np.array([msg.position.x, msg.position.y, msg.position.z], dtype=float).reshape((3, 1)) 
+
+
+  def _ned_pos_cb(self, msg : PointStamped) -> None:
+    msg_timestamp = msg.header.stamp
+
+    if not utilities.is_new_msg_timestamp(self.position_timestamp, msg_timestamp):
+      # Old message
+      return
+
+    if self.last_rotation_matrix_body_to_vehicle is None:
+      # Impossible to convert positions to body frame
+      return
+    
+    # The MPC-solver has the equations for position implemented in NED, but due to the assumption 
+    # that yaw could be neglected, the positions are technically given in body. This means that one must know
+    # the yaw angle to convert correctly
+    # Luckily in the simulator, this is known exactly
+    self.position_timestamp = msg_timestamp
+    self.position = self.last_rotation_matrix_body_to_vehicle.T @ np.array([msg.point.x, msg.point.y, msg.point.z], dtype=float).reshape((3, 1)) 
 
 
   def _attitude_cb(self, msg : QuaternionStamped) -> None:
@@ -121,16 +149,16 @@ class ModelPredictiveController():
     self.attitude_timestamp = msg_timestamp
     rotation = Rotation.from_quat([msg.quaternion.x, msg.quaternion.y, msg.quaternion.z, msg.quaternion.w])
     self.attitude_rpy = rotation.as_euler('xyz', degrees=False).reshape((3, 1))
+    self.last_rotation_matrix_body_to_vehicle = rotation.as_matrix()
 
   
   def _get_current_state_estimate(self) -> np.ndarray:
-    if  self.ekf_timestamp is None or self.velocities_timestamp is None or self.attitude_timestamp is None:
+    if  self.position_timestamp is None or self.velocities_timestamp is None or self.attitude_timestamp is None:
       return np.zeros((self.nx, self.m))
     
     return np.vstack(
       [
         self.position,
-        self.velocities,
         self.attitude_rpy
       ]
     )
@@ -152,12 +180,11 @@ class ModelPredictiveController():
 
 
   def spin(self) -> None:
-    x = np.zeros((9, 1))
     # self.is_controller_active = True
     while not rospy.is_shutdown():
       if self.is_controller_active:
         x = self._get_current_state_estimate()
-        u = self.MPC_solver.make_step(x)
+        u = self.mpc_solver.make_step(x)
         u = self._prevent_underflow(arr=u, min_value=1e-6)
 
         attitude_cmd_msg = AttitudeCommand()
@@ -168,8 +195,6 @@ class ModelPredictiveController():
         attitude_cmd_msg.gaz = u[3]
 
         self.attitude_ref_pub.publish(attitude_cmd_msg)
-      else:
-        x = np.zeros((9, 1))
 
       self.rate.sleep()
 
